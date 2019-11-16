a58f3a56332929ef410f0ece713a412fdc4c20e8
Cataline Baert <snuf_kin@yahoo.com>
2019-11-16 14:49:10 +0100

    add comments

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ const port = 3000;
 
 const noises = {'pig': 'Oink', 'cow': 'Moo', 'dog': 'Woof Woof!'}
 
+// route '/' renders 'Hi there, welcome to my assignment!'
 app.get('/', (req, res) => res.send('Hi there, welcome to my assignment!'));
 
+// route '/speak/pig' renders "The pig says 'Oink'"
+// route '/speak/cow' renders "The cow says 'Moo'"
+// route '/speak/dog' renders "The dog says 'Woof Woof!'"
 app.get('/speak/:animal', (req, res) => {
   const animal = req.params['animal'];
   res.send(`The ${animal} says '${noises[animal]}'`);
 });
 
+// route '/repeat/hello/5' renders 'hello hello hello hello hello'
+// route '/repeat/blah/2' renders 'blah blah'
 app.get('/repeat/:something/:times', (req, res) => {
   const something = `${req.params['something']} `;
   const times = req.params['times'];
