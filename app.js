29bd510d57ec80ff946eb5a3d35943884ad4df8c
Cataline Baert <snuf_kin@yahoo.com>
2019-11-16 13:58:51 +0100

    add errors handling (404, 500)

--- app.js
+++ app.js
@@ -4,4 +4,15 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// To handle unexisting routes (404 response)
+app.use(function(req, res, next) {
+  res.status(404).send("sorry can't find that!")
+});
+
+// To handle errors (500 response)
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
